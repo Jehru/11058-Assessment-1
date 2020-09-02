b539b82db8819067b167a823ed630d557e2d7752
Jehru <jehru.e.harris@gmail.com>
2020-09-02 18:13:56 +1000

    Fixed the issue with update tasks not working

--- public/update-task.php
+++ public/update-task.php
@@ -7,122 +7,116 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-?>
 
-<?php 
-    // Includes the config file that sets up the server
-    // Also includes the common.php which adds in commonly used functions
-    require "../config.php";
-    require "common.php";
+// Includes the config file that sets up the server
+// Also includes the common.php which adds in commonly used functions
+require "../config.php";
+require "common.php";
 
-    $newid = $_SESSION['id'];
-    $msgSuccess = '';
+// Create a variable for newid and the success message. These will be used later.
+$newid = $_SESSION['id'];
+$msgSuccess = '';
 
 
-    // Run when submit button is clicked
-    if (isset($_POST['submit'])) {
-        try {
-            $connection = new PDO($dsn, $username, $password, $options);  
-            
-            //grab elements from form and set as varaible
-            $work =[
-                // "userid"   => $_SESSION['id'],
-                "taskid"   => $_POST['taskid'],
-                "taskname" => $_POST['taskname'],
-                "duedate"  => $_POST['duedate'],
-                "status"   => $_POST['status'],
-                "priority" => $_POST['priority'],
-                "notes"    => $_POST['notes'],
-                "date"     => $_POST['date'],
-            ];
+// Updates the new information
+// Run when submit button is clicked
+if (isset($_POST['submit'])) {
+    try {
+
+        // Create a PDO connection which connects to the database
+        $connection = new PDO($dsn, $username, $password, $options);  
         
-            // create SQL statement
-            $sql = "UPDATE `tasks`
-                    SET taskid = :taskid, 
-                        taskname = :taskname, 
-                        duedate = :duedate, 
-                        status = :status, 
-                        priority = :priority, 
-                        notes = :notes, 
-                        date = :date 
-                    WHERE taskid = :taskid";
+        // Get the elements from the forms and set them as variables
+        $work =[
+            "taskid"   => $_POST['taskid'],
+            "taskname" => $_POST['taskname'],
+            "duedate"  => $_POST['duedate'],
+            "status"   => $_POST['status'],
+            "priority" => $_POST['priority'],
+            "notes"    => $_POST['notes'],
+            "date"     => $_POST['date'],
+        ];
+ 
+        // Create SQL statement. Which updates the information into tasks 
+        $sql = "UPDATE tasks
+                SET taskid = :taskid, 
+                    taskname = :taskname, 
+                    duedate = :duedate, 
+                    status = :status, 
+                    priority = :priority, 
+                    notes = :notes, 
+                    date = :date 
+                WHERE taskid = :taskid";
 
-            //prepare sql statement
-            $statement = $connection->prepare($sql);
-            
-            //execute sql statement
-            $statement->execute($work);
+        // Prepare the SQL statement
+        $statement = $connection->prepare($sql);
+        
+        // Execute the SQL statement
+        $statement->execute($work);
 
-            $msgSuccess = "<script> $(function(success) {
-                alertify.set('notifier','position', 'bottom-right');
-                alertify.success('Successfully Updated', 'success', 5 + alertify.get('notifier','position'));
-            }); </script>";
+        // Create a green success message using Alertify JS 
+        $msgSuccess = "<script> $(function(success) {
+            alertify.set('notifier','position', 'bottom-right');
+            alertify.success('Successfully Updated', 'success', 5 + alertify.get('notifier','position'));
+        }); </script>";
 
-            // sleep(6);
-            // header("location: welcome.php");
 
-        } catch(PDOException $error) {
-            echo $sql . "<br>" . $error->getMessage();
-        }
+    } catch(PDOException $error) {
+        // If there is an error tell us what it is
+        echo $sql . "<br>" . $error->getMessage();
     }
+}
 
-    // GET data from DB
-    //simple if/else statement to check if the id is available
-    if (isset($_GET['taskid'])) {
-        //yes the id exists 
 
-        try {
-            // standard db connection
-            $connection = new PDO($dsn, $username, $password, $options);
-            
-            // set taskid as variable
-            $taskid = $_GET['taskid'];
-            
-            //select statement to get the right data
-            // $sql = "SELECT * FROM tasks WHERE taskid = :taskid";
-            $sql = "SELECT * FROM tasks WHERE userid='$newid'";
+// Does this taskid exist and find the right information 
+if (isset($_GET['taskid'])) {
 
-            // prepare the connection
-            $statement = $connection->prepare($sql);
-            
-            //bind the id to the PDO id
-            $statement->bindValue(':taskid', $taskid);
-            
-            // now execute the statement
-            $statement->execute();
-            
-            // attach the sql statement to the new work variable so we can access it in the form
-            $work = $statement->fetch(PDO::FETCH_ASSOC);
-            
-        } catch(PDOExcpetion $error) {
-            echo $sql . "<br>" . $error->getMessage();
-        }
-    } else {
-        // no id, show error
-        echo "No Task id - something went wrong";
-        //exit;
-    };
+    try {
+        // Create a PDO connection which connects to the database
+        $connection = new PDO($dsn, $username, $password, $options);
+        
+        // Set taskid as variable
+        $taskid = $_GET['taskid'];
+        
+        // Select statement to get the right data
+        $sql = "SELECT * FROM tasks WHERE taskid= :taskid";
+
+        // Prepare the connection
+        $statement = $connection->prepare($sql);
+        
+        // Bind the id to the PDO id
+        $statement->bindValue(':taskid', $taskid);
+        
+        // Now execute the statement
+        $statement->execute();
+        
+        // Attach the sql statement to the new work variable so we can access it in the form
+        $work = $statement->fetch(PDO::FETCH_ASSOC);
+                
+    } catch(PDOExcpetion $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+} else {
+    // no id, show error
+    echo "No Task id - something went wrong";
+    exit;
+}
 
 
 ?>
 
+<!-- Include the header template -->
 <?php include "templates/header.php"; ?>
 
-<?php //if (isset($_POST['submit'])) { 
-    // Sleep 1 seconds and then send to welcome page and end the if statement
-
-    //echo "isset post submit";
-//} 
-?>
-
-
+<!-- Use the wrapper class on the main element -->
 <main class="wrapper-update">
 
+<!-- Heading -->
 <h2>Edit a work</h2>
 
-<!-- <form method="post" class="update-forms"> -->
-    <form method="post">
-
+<!-- Form that show the information. This allows the user to edit the information -->
+<!-- Uses bootstrap class names  -->
+<form method="post">
     <div class="form-group">
         <label for="taskid">Task ID</label>
         <input class="form-control" type="text" name="taskid" id="taskid" value="<?php echo escape($work['taskid']); ?>" >
@@ -152,8 +146,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         </select>
     </div>
 
-    <!-- <input class="update-input" type="text" name="priority" id="priority" value="<?php //echo escape($work['priority']); ?>"> -->
-    
     <div class="form-group">
         <label for="notes">Notes</label>
         <input class="update-input" type="text" name="notes" id="notes" value="<?php echo escape($work['notes']); ?>">
@@ -163,14 +155,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <label for="date">Input Date</label>
         <input class="update-input" type="text" name="date" id="date" value="<?php echo escape($work['date']); ?>">
     </div>
-    
+
+    <!-- Submit button -->
     <input class="update-submit" type="submit" name="submit" value="Save Changes">
 
+    <!--If the form has been sumbitted then send the AlertifyJS success message mentioned above-->
     <?php if ($msgSuccess): ?>
         <p><?=$msgSuccess?></p>
     <?php endif; ?>
-
 </form>
 
 
+<!-- Include the footer template -->
 <?php include "templates/footer.php"; ?>
